988561ffc315c033751187e6d3df23cc6a3bb1ab
AAVEGKAUSHIK <aavegkaushik@gmail.com>
2026-05-12 04:23:25 +0530

    update cors

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:5173", "https://zusko.in"],
+  credentials: true
+}));
 app.use(express.json());
 app.use("/api/health", (req, res) => {
   res.send("API is running.")
